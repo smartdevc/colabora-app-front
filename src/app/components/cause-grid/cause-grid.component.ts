c2dd7e7744ed81e29ee8b14c8abf178e6e036df5
Sebastian Droguett <sdroguett10@gnail.com>
2024-02-09 01:02:06 -0300

    Components creation

---
+++ src/app/components/cause-grid/cause-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cause-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './cause-grid.component.html',
+  styleUrl: './cause-grid.component.css'
+})
+export class CauseGridComponent {
+
+}
---
+++ src/app/components/cause-modal/cause-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cause-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './cause-modal.component.html',
+  styleUrl: './cause-modal.component.css'
+})
+export class CauseModalComponent {
+
+}
