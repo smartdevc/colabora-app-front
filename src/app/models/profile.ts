f2d14f6e26a8a834774e09bf5e716617bbd3a0a2
daniel <daniel.soto48@inacapmail.cl>
2024-03-03 00:01:22 -0300

    models created

---
+++ src/app/models/profile.ts
@@ -0,0 +1,24 @@
+import { model } from "@angular/core";
+
+class Profile {
+
+    private _id!: number;
+
+    private _name!: string;
+
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+
+    
+}
---
+++ src/app/models/project.ts
@@ -0,0 +1,43 @@
+import { model } from "@angular/core";
+
+class Project {
+
+    private _id!: number;
+
+    private _name!: string;
+
+    private _dateStart!: string;
+
+    private _dateEnd!: string;
+    
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+
+    
+    public get dateStart(): string {
+        return this._dateStart;
+    }
+    public set dateStart(value: string) {
+        this._dateStart = value;
+    }
+
+    
+    public get dateEnd(): string {
+        return this._dateEnd;
+    }
+    public set dateEnd(value: string) {
+        this._dateEnd = value;
+    }
+}
